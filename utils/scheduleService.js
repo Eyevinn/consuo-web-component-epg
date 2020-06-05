6550a0fe5b0c51c813dd16a6840a385a70b96caa
Erik Hoffman <erik.johansson87@gmail.com>
2020-06-05 12:27:07 +0200

    don't send start point for epg call

--- utils/scheduleService.js
+++ utils/scheduleService.js
@@ -1,7 +1,5 @@
 export const update = async (apiUrl, channelId) => {
-  const now = Date.now();
-  const startPoint = now - 3600 * 1000;
-  const url = `${apiUrl}/channels/${channelId}/schedule?start=${startPoint}`;
+  const url = `${apiUrl}/channels/${channelId}/schedule}`;
   const response = await fetch(url);
   const schedule = await response.json();
   return schedule || [];
